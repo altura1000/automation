fd5e06a57ba39b72d265736bf6779d2d79c95524
altura1000 <altura11111@gmail.com>
2026-07-23 00:08:47 +0300

    config github page deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = 'automation'
+// const isProduction = process.env.NODE_ENV === 'production'
+
+const isProduction = true;
+
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+
+  basePath: isProduction ? `/${repoName}` : '',
+  assetPrefix: isProduction ? `/${repoName}/` : '',
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
